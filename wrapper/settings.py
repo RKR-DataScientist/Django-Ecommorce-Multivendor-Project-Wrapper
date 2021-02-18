f97ead23610ec67db4a9d315b526fdb05738dd80
Ravi Kumar Rahul <ravikrrahul0@gmail.com>
2021-02-18 11:56:19 +0530

    4th commit
    for the heroku

--- wrapper/settings.py
+++ wrapper/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,6 +127,7 @@ USE_TZ = True
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
